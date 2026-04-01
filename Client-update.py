da3068354ea442148c96fe3615af7969be41a0e4
spike1818 <90467442+spike1818@users.noreply.github.com>
2026-04-01 13:11:09 -0400

    Update Client-update.py

--- Client-update.py
+++ Client-update.py
@@ -1,19 +1,21 @@
 import socket
 import threading
 import sys
+import struct
 
 MSG_ENCODING = "utf-8"
 
 class Client:
 
     HOSTNAME = "127.0.0.1"
+    room_array = list()
 
     def __init__(self):
         self.connected_tcp = False
         self.connected_chat = False
         self.chat_name = "Unknown"
-        self.chat_room = {}
         self.client_main()
+        #self.room_array = list()
 
     def client_main(self):
         print("starting")
@@ -33,6 +35,21 @@ class Client:
                         if len(command) > 1:
                             self.chat_name = command[1]
                             print("Name = ", self.chat_name)
+                    
+                    elif command[0] == "chat":
+                        print("Recognized chat command")
+                        if len(command) > 1:
+                            print("about to run multicast command")
+                            #room = command_split[1]
+                            for room in Client.room_array:
+                                if room[0] == command[1]:
+                                    self.multicast_chat(room[0],room[1],int(room[2]))
+                            '''
+                            if room in self.chat_room:
+                                ip, port = self.chat_room[room]
+                                self.multicast_chat(room, ip, int(port))
+                            else:
+                                print("Not in Room, Please Join it First")'''
 
                     
 
@@ -40,7 +57,7 @@ class Client:
                 command = input("Connected to server, please enter your command: ")
                 command_split = command.split(" ")
 
-                if command_split[0] == "getdir" or command_split[0] == "makeroom" or command_split[0] == "deleteroom":
+                if command_split[0] == "makeroom" or command_split[0] == "deleteroom":
                     pkt = command.encode(MSG_ENCODING)
                     self.socket.sendall(pkt)
 
@@ -51,6 +68,22 @@ class Client:
                         break
                     
                     print(data.decode(errors='ignore'), end='')
+                
+                if command_split[0] == "getdir":
+                    pkt = command.encode(MSG_ENCODING)
+                    self.socket.sendall(pkt)
+
+                    #print the recieved message from the server
+                    data = self.socket.recv(1024)
+                    if not data:
+                        self.disconnect_from_server()
+                        break
+                    print(data.decode(errors='ignore'), end='')
+                    directory_data = data.decode(errors='ignore')
+                    directory_array = directory_data.strip().split("\n")
+                    for item in directory_array:
+                        Client.room_array.append(item.split(" "))
+                    print(Client.room_array)
 
                 elif command_split[0] == "bye":
                     self.disconnect_from_server()
@@ -59,16 +92,6 @@ class Client:
                     if len(command_split) > 1:
                         self.chat_name = command_split[1]
                         print("Change Name = ", self.chat_name)
-
-                elif command_split[0] == "chat":
-                    if len(command_split) > 1:
-                        room = command_split[1]
-
-                        if room in self.chat_room:
-                            ip, port = self.chat_room[room]
-                            self.multicast_chat(room, ip, int(port))
-                        else:
-                            print("Not in Room, Please Join it First")
 
         
 
@@ -79,21 +102,22 @@ class Client:
         self.connected_chat = True
 
         try:
-            self.chat_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.chat_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
             self.chat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.chat_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
             self.chat_socket.bind(('', port))
 
-            group = socket.inet_aton(multicast_ip)
-            
-            iface = socket.inet_aton("0.0.0.0")
-            mreq = group + iface
+            mreq = struct.pack(
+                "4s4s",
+                socket.inet_aton(multicast_ip),
+                socket.inet_aton("0.0.0.0"),
+            )
 
             self.chat_socket.setsockopt(
                 socket.IPPROTO_IP,
                 socket.IP_ADD_MEMBERSHIP,
                 mreq
             )
+            self.chat_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 
         except Exception as msg:
             print("Multicast chat error:", msg)
